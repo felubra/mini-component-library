d93d030c3f76660d23c1d65df7ee868a242b1b4e
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-01-29 21:48:58 -0300

    feat: ProgressBar component
    Squashed commit of the following:
    
    commit cb44543d9715a63eb8a62a4ac8655d4f3d637d5e
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 21:37:56 2022 -0300
    
        fix: min can be equal to max
    
    commit d34c9d6cc13ecc4f05a108f48bc4433f744c26b3
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 21:34:55 2022 -0300
    
        feat: render percentage visually hidden, rounded to two decimal cases
    
    commit f720367775b210eb9b1bb14e6790f33c78112add
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 21:15:41 2022 -0300
    
        feat: add aria properties
    
    commit af0cbcce113ab9439495550ab2ef05f1b7182341
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 21:15:16 2022 -0300
    
        feat: use a limited value between max and min
    
    commit 4bb3e7e5b99ef5440facdc47ee87eb7b6f03fff1
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 21:13:33 2022 -0300
    
        refactor: rename variable
    
    commit ff62613c9adf40399b83af002f103e53308a230e
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 21:12:27 2022 -0300
    
        feat: validate the min and max value props
    
    commit 29e5f4cbdc240204eb8b451a69d129b113a40172
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 21:11:27 2022 -0300
    
        feat: accept a min and a max value
    
    commit c12f3661f1572dc0a51332207cde1b13f091d80a
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 20:33:14 2022 -0300
    
        refactor: use color constants
    
    commit eb8da3dc0560d1fd8e5a6e13d43b5c8eeb2d54e3
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 20:19:49 2022 -0300
    
        feat: add a transition to animate the scrollbar fulfilling
    
    commit cf034545eb474ef8ebda71c90d65a0de971a0f88
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 20:18:59 2022 -0300
    
        feat: use scale transform instead of chaging width
    
    commit 5d65b6573380b2f6e6776c50236d51b3b2c14eec
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 20:13:55 2022 -0300
    
        feat: use css vars to control the appearance of the component
    
    commit 46114bda0127bd21f912f6625d249728bf5991d7
    Author: Felipe Lube de Bragança <felipe.lubra@gmail.com>
    Date:   Sat Jan 29 20:13:06 2022 -0300
    
        chore: keep a public folder, as it is needed for storybook to build properly

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,86 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const VARIANT_STYLES = {
+  large: {
+    "--inner-bar-height": "16px",
+    "--inner-bar-wrapper-border-radius": "6px",
+    "--wrapper-border-radius": "8px",
+    "--wrapper-padding": "4px",
+  },
+  medium: {
+    "--inner-bar-height": "12px",
+    "--inner-bar-wrapper-border-radius": "3px",
+    "--wrapper-border-radius": "4px",
+    "--wrapper-padding": "0",
+  },
+  small: {
+    "--inner-bar-height": "8px",
+    "--inner-bar-wrapper-border-radius": "3px",
+    "--wrapper-border-radius": "4px",
+    "--wrapper-padding": "0",
+  },
+};
+
+const InnerBar = styled.div`
+  background: ${COLORS.primary};
+  border-radius: 4px 0px 0px 4px;
+
+  height: var(--inner-bar-height, 16px);
+  transition: transform cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
+`;
+
+const InnerBarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: var(--inner-bar-wrapper-border-radius);
+
+  ${InnerBar} {
+    transform-origin: 0 0;
+    transform: ${(props) => `scaleX(${props.value}%)`};
+  }
+`;
+
+const Wrapper = styled.div`
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--wrapper-border-radius);
+  padding: var(--wrapper-padding);
+  overflow: hidden;
+`;
+
+const ProgressBar = ({ value, size, min = 0, max = 100 }) => {
+  if (isNaN(min) || isNaN(max)) {
+    throw new Error("The min and max props needs to be numbers");
+  }
+
+  if (min > max) {
+    throw new Error("The min prop needs to be less than or equal the max prop");
+  }
+
+  const limitedValue = Math.max(Math.min(value, max), min);
+  const progress = ((limitedValue - min) / (max - min)) * 100;
+  const roundedProgress = Math.round((progress + Number.EPSILON) * 100) / 100;
+
+  return (
+    <Wrapper
+      style={VARIANT_STYLES[size]}
+      role="progressbar"
+      aria-valuenow={limitedValue}
+      aria-valuemin={min}
+      aria-valuemax={max}
+    >
+      <InnerBarWrapper value={progress}>
+        <>
+          <InnerBar />
+          <VisuallyHidden>{`${roundedProgress}%`}</VisuallyHidden>
+        </>
+      </InnerBarWrapper>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
